b8cf9e936adf0bb27f528d3a8ec4df65c5081d19
Putinjov <putinjov@gmail.com>
2023-05-27 00:14:04 +0300

    base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Home from "pages/Home";
+import Movies from "pages/Movies";
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>Home</StyledLink>
+        <StyledLink to="/movies">Movies</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
